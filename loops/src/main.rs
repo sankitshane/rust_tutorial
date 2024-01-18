fcc3a7af6902f9bac4e4b1b9eb5725828d785d10
sankitshane <sankit.acharya@gmail.com>
2024-01-19 00:20:26 +0530

    chore: Add project 3

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,17 +1,22 @@
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
     println!("Working on mini projects");
 
-    println!("Project 1");
-    println!("Convert fahrenheit_to_celsius");
-    let temp = fahrenheit_to_celsius();
-    println!("Temperture in Celsius: {temp} ");
+    // println!("Project 1");
+    // println!("Convert fahrenheit_to_celsius");
+    // let temp = fahrenheit_to_celsius();
+    // println!("Temperture in Celsius: {temp} ");
 
-    println!("Project2");
-    println!("Get the nth fibonacci number");
-    let res = fibonacci();
-    println!("The nth fibonacci number is {res}");
+    // println!("Project 2");
+    // println!("Get the nth fibonacci number");
+    // let res = fibonacci();
+    // println!("The nth fibonacci number is {res}");
+
+    println!("project 3");
+    println!("Generate The Twelve Days of Christmas");
+    christmas_song();
 }
 
 fn fahrenheit_to_celsius() -> f32 {
@@ -39,4 +44,30 @@ fn fibonacci() -> i32 {
     let n = input.trim().parse().expect("Not a valid number");
 
     return dp(n);
+}
+
+fn christmas_song() {
+    fn generate_phase(n: i32){
+        // sending n from 0-2
+        let number = [
+            "first",
+            "second",
+            "third"
+        ];
+
+        let mut num_name = HashMap::from([
+            ("Two", "turtle"),
+            ("Three", "French"),
+        ]);
+
+        let mut part = Vec::new();
+        part.push("On the {number[n]} day of Christmas".to_string());
+        part.push("my true love gave to me".to_string());
+        
+        part.push("A partridge in a pear tree.".to_string());
+    }
+    
+    for num in 0..3 {
+        generate_phase(num);
+    }
 }
