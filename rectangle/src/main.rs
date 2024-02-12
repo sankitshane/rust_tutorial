795a3e63747a64a4f18cb1591ee9c077908282aa
sankitshane <sankit.acharya@gmail.com>
2024-02-12 11:58:25 +0530

    feat: update variables to tuple type

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rectangle = (30, 50);
 
-    println!("The area of the rectangle is: {}", area(width1, height1));
+    println!("The area of the rectangle is: {}", area(rectangle));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
