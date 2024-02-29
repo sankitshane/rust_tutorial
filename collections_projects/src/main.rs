7ad0401e706aa3f3c418e7e9dcc75bfda53774da
sankitshane <sankit.acharya@gmail.com>
2024-02-29 13:29:15 +0530

    feat: Create project-3 to use HashMap and Vectors

--- collections_projects/src/main.rs
+++ collections_projects/src/main.rs
@@ -17,6 +17,9 @@ fn main() {
     println!(
         "The result for project are {input_1}: {result_1} & {input_2}: {result_2}"
     );
+
+    println!("Project-3: Store employees by department");
+    store_departments();
 }
 
 fn find_median_and_mode(input: &mut [i32]) -> (i32, i32) {
@@ -66,4 +69,37 @@ fn generate_pig_latin(input: &String) -> String {
     }
 
     
+}
+
+fn store_departments() {
+    let mut register: HashMap<String, Vec<String>> = HashMap::new();
+
+    let s_1 = String::from("Add Sally to Engineering");
+    let s_2 = String::from("Add Amir to Sales");
+    let s_3 = String::from("Add Sankit to Engineering");
+    add_employee(s_1, &mut register);
+    add_employee(s_2, &mut register);
+    add_employee(s_3, &mut register);
+
+    println!("Current register: {:?}", register);
+
+    let d_1 = String::from("Engineering");
+    let d_2 = String::from("Sales");
+    emp_by_department(d_1, &register);
+    emp_by_department(d_2, &register);
+}
+
+fn add_employee(sentence : String, register: &mut HashMap<String, Vec<String>>) {
+    let parts : Vec<&str> = sentence.split(" ").collect();
+    let emp_name = &parts[1].to_string();
+    let emp_department = parts[3].to_string();
+    let _ = &register.entry(emp_department)
+            .and_modify(|v| v.push((&emp_name).to_string()))
+            .or_insert(vec![(&emp_name).to_string()]);
+}
+
+fn emp_by_department(department: String, register: &HashMap<String, Vec<String>>) {
+    if let Some(emp_names) = &register.get(&department) {
+        println!("{:?}", emp_names);
+    }
 }
